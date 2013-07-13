03128b836fe1132668376eb2e2e1a25cf78d4112
wp2123 <wangpu2123@sina.com>
2013-07-13 10:47:53 +0800

    and

--- spec/ut/demo02/digest_spec.js
+++ spec/ut/demo02/digest_spec.js
@@ -7,10 +7,26 @@ describe("demo02 digest test", function () {
     }));
 
     it("should return 0 if no change", function () {
-        scope.$watch("name", function (new_value, old_value) {
+        scope.$watch("value", function (new_value, old_value) {
             scope.count = scope.count + 1;
         });
         expect(scope.count).toEqual(0);
     });
 
+    it("should return 0 if change value but no digest", function () {
+        scope.$watch("value", function (new_value, old_value) {
+            scope.count = scope.count + 1;
+        });
+        scope.value = "321";
+        expect(scope.count).toEqual(0);
+    });
+
+    it("should return 1 if change value and digest", function () {
+        scope.$watch("value", function (new_value, old_value) {
+            scope.count = scope.count + 1;
+        });
+        scope.value = "321";
+        scope.$digest();         //出发改动
+        expect(scope.count).toEqual(1);
+    });
 });
